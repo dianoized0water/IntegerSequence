aab04241b8c7bafbe7d3dcb58b464e5be7022a80
diana_yang <dyang20@stuy.edu>
2020-12-04 13:05:22 -0500

    has errors on the ElementExceptions, will fix later

--- Range.java
+++ Range.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.NoSuchElementException;
 
 public class Range implements IntegerSequence{
   private int start, end, current;
@@ -14,5 +14,23 @@ public class Range implements IntegerSequence{
 
   public int length(){
     return (end - start + 1);
+  }
+
+  public boolean hasNext(){
+    if (current<end){
+      return true;
+    }
+    else{
+      throw new NoSuchElementException();
+    }
+  }
+
+  public int next(){
+    int a = 0;
+    if (hasNext()){
+      current++;
+      a = current - 1;
+    }
+    return a;
   }
 }
