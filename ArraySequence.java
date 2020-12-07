fb98727457cd517660da48739128c561ba020a4f
diana_yang <dyang20@stuy.edu>
2020-12-07 13:07:58 -0500

    one final commit

---
+++ ArraySequence.java
@@ -0,0 +1,45 @@
+import java.util.NoSuchElementException;
+
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int[] data;
+
+  public ArraySequence(int []other){
+    currentIndex = 0;
+    data = other;
+  }
+
+  public ArraySequence(IntegerSequence otherseq){
+    int a = 0;
+    otherseq.reset();
+    while (otherseq.hasNext()){
+      otherseq.next();
+      a++;
+    }
+    otherseq.reset();
+    data = new int[a];
+    for (int i=0; i<a; i++){
+      data[i] = otherseq.next();
+    }
+    currentIndex = 0;
+    otherseq.reset();
+  }
+
+  public boolean hasNext(){
+    return currentIndex<data.length;
+  }
+
+  public int next(){
+    if(hasNext()){
+      currentIndex++;
+      return data[currentIndex - 1];
+    }
+    else{
+      throw new NoSuchElementException();
+    }
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+}
